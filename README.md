a26b146554686268db56bb51b5ccdd66e872b8fd
Five Grant <5@fivegrant.com>
2023-10-04 13:23:36 -0500

    Complete initial version (#8)
    * Publish to S3 successfully
    
    * Simplify docker compose around pyciemss
    
    * Simplify name
    
    * Tweaks for approval
    
    * Make uploading explicit
    
    * Fix id in example
    
    * Fix logging order
    
    * Print JSON payload on fail
    
    * docker config updates
    
    * Update optimize calibrate to use right dataset
    
    * Fix solution mappings
    
    * wrapping up the wrapup
    
    ---------
    
    Co-authored-by: Matthew Printz <matt@jataware.com>

--- README.md
+++ README.md
@@ -4,7 +4,18 @@ standing up all of the relevant TA3 services with a Docker Compose
 and trying to use a few requests through the relevant parts of the stack.
 
 ## Usage
-Run `docker compose up`. Once the `tests` container completes, the report is done.
+Run `docker compose run --build tests`. Once the `tests` container completes, the report is done.
+
+If run with UPLOAD=TRUE as an environment variable, the report will be uploaded to S3.
+Otherwise, the report is output as part of the `tests` container's logs.
+
+The logs can be viewed by running `docker compose logs -f tests`.
+
+Note: The services used in testing are not cleaned up following testing. When done with running tests,
+be sure to shut down the services by running `docker compose down` to conserve your resources.
+
+See `env.sample` to see which environment variables are used by the test/report runner. 
+You can create a `.env` envfile based on sample if you want to override the defaults.
 
 ## Adding Scenarios
 To add scenarios, create a new directory in `scenarios`. For each request you would like to 
--- tests/report.py
+++ tests/report.py
@@ -1,25 +1,31 @@
 import os
 import json
+import logging
 from time import sleep, time
 from datetime import datetime
+from collections import defaultdict
 
 import boto3
 import requests
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
 
 TDS_URL = os.environ.get("TDS_URL", "http://data-service:8000")
 PYCIEMSS_URL = os.environ.get("PYCIEMSS_URL", "http://pyciemss-api:8000")
 SCIML_URL = os.environ.get("SCIML_URL", "http://sciml-service:8080")
 BUCKET = os.environ.get("BUCKET", None)
+UPLOAD = os.environ.get("UPLOAD", "FALSE").lower() == "true"
 
 
 def eval_integration(service_name, endpoint, request):
     start_time = time()
     is_success = False
     base_url = PYCIEMSS_URL if service_name == "pyciemss" else SCIML_URL
-    kickoff_request = requests.post(f"{base_url}/{endpoint}", json=request, 
+    kickoff_request = requests.post(f"{base_url}/{endpoint}", json=request,
         headers= {
             "Content-Type": "application/json"
-        }    
+        }
     )
     if kickoff_request.status_code < 300:
         sim_id = kickoff_request.json()["simulation_id"]
@@ -37,9 +43,9 @@ def eval_integration(service_name, endpoint, request):
 def gen_report():
     report = {
         "scenarios": {
-            "pyciemss": {},
-            "sciml": {}
-        }, 
+            "pyciemss": defaultdict(dict),
+            "sciml": defaultdict(dict)
+        },
         "services": {
             "TDS": {
                 "version": "UNAVAILABLE"
@@ -53,9 +59,9 @@ def gen_report():
         }
     }
 
-    
-    report["scenarios"] = {name: {} for name in os.listdir("scenarios")}
-    for scenario in report["scenarios"]:
+
+    scenarios = {name: {} for name in os.listdir("scenarios")}
+    for scenario in scenarios:
         scenario_spec = {}
         for backend in ["pyciemss", "sciml"]:
             path = f"scenarios/{scenario}/{backend}"
@@ -64,16 +70,15 @@ def gen_report():
         for service_name, tests in scenario_spec.items():
             for test_file in tests:
                 test = test_file.split(".")[0]
-                name = f"{service_name}-{test}"
-                file = open(f"scenarios/{scenario}/{service_name}/{test_file}", "rb") 
-                print(f"Trying `/{endpoint}` ({service_name}, {scenario})")
-                report["scenarios"][scenario][name] = eval_integration(service_name, test, json.load(file))
-                print(f"Completed `/{endpoint}` ({service_name}, {scenario})")
+                file = open(f"scenarios/{scenario}/{service_name}/{test_file}", "rb")
+                logging.info(f"Trying `/{test}` ({service_name}, {scenario})")
+                report["scenarios"][service_name][scenario][test] = eval_integration(service_name, test, json.load(file))
+                logging.info(f"Completed `/{test}` ({service_name}, {scenario})")
     return report
 
 
 def publish_report(report, upload):
-    print("Publishing report")
+    logging.info("Publishing report")
     timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
     filename = f"report_{timestamp}.json"
     fullpath = os.path.join("reports", filename)
@@ -81,11 +86,18 @@ def publish_report(report, upload):
     with open(fullpath, "w") as file:
         json.dump(report, file, indent=2)
 
-    if upload:
-        print("Uploading report")
+    if upload and BUCKET is not None:
+        logging.info(f"Uploading report to '{BUCKET}'")
         s3 = boto3.client("s3")
         full_handle = os.path.join("ta3", filename)
         s3.upload_file(fullpath, BUCKET, full_handle)
+
+    elif upload and BUCKET is None:
+        logging.error("NO BUCKET WAS PROVIDED. CANNOT UPLOAD")
+
+    if not upload or BUCKET is None:
+        logging.info(f"{fullpath}:")
+        logging.info(open(fullpath, "r").read())
 
 
 def report(upload=True):
@@ -93,4 +105,4 @@ def report(upload=True):
 
 
 if __name__ == "__main__":
-    report(BUCKET is not None)
+    report(UPLOAD)
