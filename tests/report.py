493d8d2814fc1a9da48aea30210bbab5df2f4d52
sabinala <130604122+sabinala@users.noreply.github.com>
2023-11-29 07:07:00 -0800

    Ignore non-JSON files in scenarios (#42)

--- tests/report.py
+++ tests/report.py
@@ -74,7 +74,8 @@ def gen_report():
         for backend in ["pyciemss", "sciml"]:
             path = f"scenarios/{scenario}/{backend}"
             if os.path.exists(path):
-                scenario_spec[backend] = os.listdir(f"scenarios/{scenario}/{backend}")
+                scenario_spec[backend] = [f for f in os.listdir(f"scenarios/{scenario}/{backend}")
+                                          if f.endswith(".json")] # only grab json files (ignore hidden notebooks)
         for service_name, tests in scenario_spec.items():
             for test_file in tests:
                 test = test_file.split(".")[0]
